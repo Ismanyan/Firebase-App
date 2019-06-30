9570dcde58281311be150f97a401a87fc472b97b
Bilkis Ismail <bilkisismail07@Gmail.com>
2019-06-30 22:55:20 +0700

    Update App

---
+++ js/firebase.js
@@ -0,0 +1,34 @@
+
+// Firebase Database Reference and the child
+const dbRef = firebase.database().ref();
+const usersRef = dbRef.child('users');
+
+// --------------------------
+// ADD
+// --------------------------
+
+const addUserBtnUI = document.getElementById("add-user-btn");
+addUserBtnUI.addEventListener("click", addUserBtnClicked)
+
+
+
+function addUserBtnClicked() {
+
+	const usersRef = dbRef.child('users');
+
+	const addUserInputsUI = document.getElementsByClassName("user-input");
+
+ 	// this object will hold the new user information
+    let newUser = {};
+
+    // loop through View to get the data for the model
+    for (let i = 0, len = addUserInputsUI.length; i < len; i++) {
+
+        let key = addUserInputsUI[i].getAttribute('data-key');
+        let value = addUserInputsUI[i].value;
+        newUser[key] = value;
+    }
+	usersRef.push(newUser)
+   alert("Berhasil Di kirim");
+
+}
